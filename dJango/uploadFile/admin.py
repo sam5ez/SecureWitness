d70cb68e85b2ed0cb831ed2ab294c36a6c6fcc6a
Qian Xiong <qx2dk@virginia.edu>
2015-04-05 07:54:19 -0400

    admin can assign users to groups; hide users' real names and reports' authors from admin

--- dJango/uploadFile/admin.py
+++ dJango/uploadFile/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from . import models
 
-admin.site.register(models.Report)
+from uploadFile import models
+
+
+class ReportAdmin(admin.ModelAdmin):
+    fields = ('title', 'short_desc', 'detailed_desc', 'location', 'file', 'tag', 'private')
+    list_display = ('title', 'private')
+
+
+admin.site.register(models.Report, ReportAdmin)
--- dJango/userAccount/admin.py
+++ dJango/userAccount/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+class MainAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('username', 'is_active', 'is_staff', 'is_superuser', "last_login", "date_joined")
+    list_editable = ('is_staff', 'is_superuser')
+
+
+admin.site.unregister(User)
+admin.site.register(User, MainAdmin)
