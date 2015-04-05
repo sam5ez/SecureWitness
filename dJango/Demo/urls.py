9d0b81c9b039c6aa84496810e569e919b5a85e54
Qian Xiong <qx2dk@virginia.edu>
2015-04-05 02:00:40 -0400

    create account function

--- dJango/Demo/urls.py
+++ dJango/Demo/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from uploadFile.views import upload_file, search_file
+from userAccount.views import create_account
 
 urlpatterns = patterns('',
                        # Examples:
@@ -9,4 +10,5 @@ urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^upload/$', upload_file),
                        url(r'^search/$', search_file),
+                       url(r'^create_account/$', create_account),
                        )
--- dJango/userAccount/views.py
+++ dJango/userAccount/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .forms import CreateAccountForm
+import datetime
 
-# Create your views here.
+
+def create_account(request):
+    if request.method == 'POST':
+        form = CreateAccountForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            User.objects.create_user(username=username, email=email, password=password, first_name=first_name,
+                                     last_name=last_name,last_login=datetime.datetime.now())
+            context = {'username': username, 'succeed': True}
+        else:
+            context = {'form': form, 'succeed': False}
+        return render(request, "create_account_result.html", context)
+    else:
+        return render(request, "create_account.html", {'form': CreateAccountForm()})
